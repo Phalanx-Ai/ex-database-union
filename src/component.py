78513c9ab8f7d0d6102bef368244294478b7776a
Marek Grac <marek@bedeep.eu>
2024-08-15 14:37:34 +0200

    [feat] Create output tables from configuration params

--- src/component.py
+++ src/component.py
@@ -4,7 +4,7 @@ Template Component main class.
 '''
 import csv
 import logging
-from datetime import datetime
+from collections import defaultdict
 
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
@@ -32,39 +32,78 @@ class Component(ComponentBase):
     def __init__(self):
         super().__init__()
 
-    def run(self):
-        '''
-        Main execution code
-        '''
+    def _get_table_structures(self):
+        KEY_TABLE_NAME = "table"
+        KEY_COLUMN_NAME = "column"
 
+        KEY_COMPANY_NAME = "_company"
+        KEY_COMPANY_YEAR = "_year"
+
+        tables = defaultdict(lambda: list([KEY_COMPANY_NAME, KEY_COMPANY_YEAR]))
+        for column in self.configuration.parameters[KEY_COLUMNS]:
+            tables[column[KEY_TABLE_NAME]].append(column[KEY_COLUMN_NAME])
+
+        return tables
+
+    def _get_unique_prefix_for_db(self):
+        # @todo: fix according to real databases name
+        # DATABASE_NAME = "foo_2024"
+        DATABASE_COMPANY = "foo"
+        DATABASE_YEAR = str(2024)
+        #
+
+        return "%s_%s_" % (DATABASE_COMPANY, DATABASE_YEAR)
+
+    def run(self):
         # check for missing configuration parameters
         self.validate_configuration_parameters(REQUIRED_PARAMETERS)
         self.validate_image_parameters(REQUIRED_IMAGE_PARS)
         params = self.configuration.parameters
-
         logging.info(params)
-        # Access parameters in data/config.json
-        if params.get(KEY_COLUMNS):
-            logging.info(params[KEY_COLUMNS])
 
-        # Create output table (Tabledefinition - just metadata)
-        table = self.create_out_table_definition('output.csv', incremental=True, primary_key=['timestamp'])
+        table_structures = self._get_table_structures()
+        for table_name in table_structures.keys():
+            # @todo: set all rows to the same destination (output can be folder?)
+            csv_filename = self._get_unique_prefix_for_db() + "%s.csv" % (table_name)
 
-        # get file path of the table (data/out/tables/Features.csv)
-        out_table_path = table.full_path
-        logging.info(out_table_path)
+            table = self.create_out_table_definition(
+                csv_filename,
+                incremental=False,
+                columns=table_structures[table_name])
+            self.write_manifest(table)
 
-        # DO whatever and save into out_table_path
-        with open(table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
-            writer = csv.DictWriter(out_file, fieldnames=['timestamp'])
-            writer.writeheader()
-            writer.writerow({"timestamp": datetime.now().isoformat()})
+            with open(table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
+                writer = csv.DictWriter(
+                    out_file,
+                    fieldnames=table_structures[table_name]
+                )
+                writer.writeheader()
 
-        # Save table manifest (output.csv.manifest) from the tabledefinition
-        self.write_manifest(table)
-
-        # Write new state - will be available next run
-        self.write_state_file({"some_state_parameter": "value"})
+                # @todo: REMOVE (insert debug data only)
+                if len(writer.fieldnames) == 4:
+                    writer.writerow({
+                       '_company': 'Phalanx',
+                       '_year': '2023',
+                       'id': 1,
+                       'suma': 1000
+                    })
+                    writer.writerow({
+                       '_company': 'Phalanx',
+                       '_year': '2023',
+                       'id': 2,
+                       'suma': 555
+                    })
+                elif len(writer.fieldnames) == 3:
+                    writer.writerow({
+                       '_company': 'Phalanx',
+                       '_year': '2023',
+                       'id': 141,
+                    })
+                    writer.writerow({
+                       '_company': 'Phalanx',
+                       '_year': '2023',
+                       'id': 333,
+                    })
 
 
 """
